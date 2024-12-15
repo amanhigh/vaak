d2a021caa17eac45719cf3353f03a063218b126b
Amanpreet Singh <amanpreet.singh@flipkart.com>
2024-12-15 16:28:29 +0530

    Review Comments

--- app/src/main/java/com/aman/vaak/handlers/VaakInputMethodService.kt
+++ app/src/main/java/com/aman/vaak/handlers/VaakInputMethodService.kt
@@ -2,16 +2,20 @@ package com.aman.vaak.handlers
 
 import android.inputmethodservice.InputMethodService
 import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import com.aman.vaak.R
 import com.aman.vaak.managers.ClipboardManager
+import com.aman.vaak.models.KeyboardState
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class VaakInputMethodService : InputMethodService() {
     @Inject lateinit var clipboardManager: ClipboardManager
+
+    private var keyboardState: KeyboardState? = null
 
     override fun onCreateInputView(): View {
         return layoutInflater.inflate(R.layout.keyboard, null).apply {
@@ -22,8 +26,18 @@ class VaakInputMethodService : InputMethodService() {
         }
     }
 
+    override fun onStartInput(info: EditorInfo?, restarting: Boolean) {
+        super.onStartInput(info, restarting)
+        keyboardState = KeyboardState(currentInputConnection, info)
+    }
+
+    override fun onFinishInput() {
+        keyboardState = null
+        super.onFinishInput()
+    }
+
     private fun handlePaste() {
-        currentInputConnection?.let { inputConnection ->
+        keyboardState?.inputConnection?.let { inputConnection ->
             clipboardManager.pasteContent(inputConnection)
         }
     }
---
+++ app/src/main/java/com/aman/vaak/models/KeyboardState.kt
@@ -0,0 +1,15 @@
+package com.aman.vaak.models
+
+import android.view.inputmethod.EditorInfo
+import android.view.inputmethod.InputConnection
+
+/**
+ * Represents the current state of the keyboard.
+ *
+ * @property inputConnection Current input connection for text manipulation
+ * @property editorInfo Information about the current input field
+ */
+data class KeyboardState(
+    val inputConnection: InputConnection?,
+    val editorInfo: EditorInfo?
+)
